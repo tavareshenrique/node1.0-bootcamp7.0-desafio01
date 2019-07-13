92e7f4c8e96e6113a1d454d6ca3f0542b1ad6a77
Henrique Tavares <ihenrits@gmail.com>
2019-07-13 19:10:23 -0300

    Incluindo Projeto'

--- index.js
+++ index.js
@@ -10,4 +10,12 @@ server.get("/projects", (req, res) => {
   return res.json(projects);
 });
 
+server.post("/projects", (req, res) => {
+  projects.push(req.body);
+
+  req.projects = req.body;
+
+  return res.json(projects);
+});
+
 server.listen(3000);
