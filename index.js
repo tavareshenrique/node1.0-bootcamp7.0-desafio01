187a042f1d27a5d3479bb4b0bece603b0241c3db
Henrique Tavares <ihenrits@gmail.com>
2019-07-13 19:09:52 -0300

    Listando Projetos e Tarefas

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ server.use(express.json());
 
 const projects = [];
 
-server.post("/projects", (req, res) => {
-  projects.push(req.body);
-
-  req.projects = req.body;
-
+server.get("/projects", (req, res) => {
   return res.json(projects);
 });
 
