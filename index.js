e82fc74a0dc827fb1ac40d0f59ce88812ab7cb9a
Henrique Tavares <ihenrits@gmail.com>
2019-07-13 19:16:07 -0300

    Adicionando Rota para Add Tasks ao Projeto

--- index.js
+++ index.js
@@ -18,6 +18,21 @@ server.post("/projects", (req, res) => {
   return res.json(projects);
 });
 
+server.post("/projects/:id/tasks", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  for (let prop in projects) {
+    const idProject = projects[prop].id;
+    if (idProject === id) {
+      projects[prop].tasks.push(title);
+      req.projects = projects;
+    }
+  }
+
+  return res.json(projects);
+});
+
 server.put("/projects/:id", (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
